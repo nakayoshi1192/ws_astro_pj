7cc1d1cbb74ab1c0a4855e0959ee7b7cfcd12c03
nakayoshi <n.kazushige@gyoku.jp>
2026-04-01 23:45:49 +0900

    github用に設定

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,7 @@ import react from '@astrojs/react';
 // https://astro.build/config
 
 export default defineConfig({
+  site: 'https://nakayoshi1192.github.io',
+  base: '/ws_astro_pj',
   integrations: [react()],
 });
---
+++ src/data/countries.js
@@ -0,0 +1,74 @@
+export const countries = [
+  {
+    id: "1",
+    name: "日本",
+    capital: "東京",
+    population: "125800000",
+    region: "アジア",
+    currency: "円"
+  },
+  {
+    id: "2",
+    name: "アメリカ",
+    capital: "ワシントンD.C.",
+    population: "331900000",
+    region: "北アメリカ",
+    currency: "ドル"
+  },
+  {
+    id: "3",
+    name: "中国", 
+    capital: "北京",
+    population: "1412000000",
+    region: "アジア",
+    currency: "人民元"
+  },
+  {
+    id: "4",
+    name: "インド",
+    capital: "ニューデリー",
+    population: "1380000000",
+    region: "アジア",
+    currency: "ルピー"
+  },
+  {
+    id: "5",
+    name: "ブラジル",
+    capital: "ブラジリア",
+    population: "214300000",
+    region: "南アメリカ",
+    currency: "レアル"
+  },
+  {
+    id: "6",
+    name: "イギリス",
+    capital: "ロンドン",
+    population: "67220000",
+    region: "ヨーロッパ",
+    currency: "ポンド"
+  },
+  {
+    id: "7",
+    name: "フランス",
+    capital: "パリ",
+    population: "67390000",
+    region: "ヨーロッパ",
+    currency: "ユーロ"
+  },
+  {
+    id: "8",
+    name: "ドイツ",
+    capital: "ベルリン",
+    population: "83240000",
+    region: "ヨーロッパ",
+    currency: "ユーロ"
+  },
+  {
+    id: "9",
+    name: "イタリア",
+    capital: "ローマ",
+    population: "59550000",
+    region: "ヨーロッパ",
+    currency: "ユーロ"
+  }
+];
